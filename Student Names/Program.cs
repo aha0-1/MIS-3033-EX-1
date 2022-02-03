e585e62c08082224872442cf6ab5f6865e43e0f0
Ann Ha <Angela.H.Ha-1@ou.edu>
2022-02-02 23:10:02 -0600

    Add project files.

---
+++ Student Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Student_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentName = new string[35];
+            string answer = string.Empty;
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Please enter student full");
+                studentName[i] = Console.ReadLine();
+                i++;//increase and store in array 
+            } while (answer == "-1");
+            {
+                Console.WriteLine(studentName.Length);
+            }
+        }
+    }
+}
